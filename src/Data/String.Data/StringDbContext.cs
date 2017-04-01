21fc8dada3b5a981657439927d7aa929c9ea4686
avg <atanasgeorgiev83@gmail.com>
2017-04-01 16:35:41 +0300

    Added data projects

---
+++ src/Data/String.Data/StringDbContext.cs
@@ -0,0 +1,59 @@
+﻿namespace String.Data
+{
+    using System;
+
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using String.Data.Common.Models;
+    using String.Data.Models;
+
+    public class MyServerDbContext : IdentityDbContext<User>
+    {
+        public MyServerDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+//        public virtual DbSet<Album> Albums { get; set; }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Album>().HasOne(p => p.Cover).WithMany(b => b.Covers);
+            //modelBuilder.Entity<Image>().HasMany(x => x.Covers).WithOne(x => x.Cover).HasForeignKey(x => x.CoverId);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            foreach (var entry in this.ChangeTracker.Entries())
+            {
+                if (entry.Entity is IAuditInfo
+                    && ((entry.State == EntityState.Added) || (entry.State == EntityState.Modified)))
+                {
+                    var entity = entry.Entity as IAuditInfo;
+                    if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                    {
+                        entity.CreatedOn = DateTime.Now;
+                    }
+                    else
+                    {
+                        entity.ModifiedOn = DateTime.Now;
+                    }
+                }
+            }
+        }
+    }
+}
